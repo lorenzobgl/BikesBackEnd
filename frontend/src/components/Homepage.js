c775dd48f478a3b7efeae0e9b1ec2b7d503c2344
gbelda <giovannimelville.belda@nttdata.com>
2022-07-13 14:13:58 +0200

    adjust homepage frontend

---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './assets/css/home.css'
+
+export default function Homepage() {
+    return (
+        <div>
+            <div className='hero'>
+                <div className='pt-5'>
+                    <h1 className='fst-italic text-light pt-5'>
+                        Rent the best bikes in Italy!
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
