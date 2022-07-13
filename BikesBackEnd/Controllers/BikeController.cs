e1127fd8672a9960830ecaffaf3e8586fff96395
GMLen94 <Gianmaria.Leni@emea.nttdata.com>
2022-07-13 14:11:12 +0200

    ModifiedBikeController

--- BikesBackEnd/Controllers/BikeController.cs
+++ BikesBackEnd/Controllers/BikeController.cs
@@ -17,36 +17,36 @@ namespace BikesBackEnd.Controllers
         [HttpGet(Name = "GetBikes")]
         public List<Bike> GetBikes()
         {
-            List<Bike> bikes= _bikeservice.getBikes();
+            List<Bike> bikes = _bikeservice.getBikes();
 
             return bikes;
         }
-        
+
         [Authorize(Roles = "Admin")]
         [HttpGet(Name = "AddBike")]
-        public IActionResult CreatedBike(IFormCollection bikeform)
+        public IActionResult CreatedBike([FromBody] Bike bikeform)
         {
             Bike bike = new Bike()
             {
                 Id = Guid.NewGuid(),
-                Type = bikeform["Type"],
-                IsWorking = bool.Parse(bikeform["IsWorking"]),
-                LockOn = bool.Parse(bikeform["LockOn"]),
-                IdStation = new Guid("d70552c0-6185-4c78-88c0-f56272678bc9"),
+                Type = bikeform.Type,
+                IsWorking = bikeform.IsWorking,
+                LockOn = bikeform.LockOn,
+                IdStation = new Guid("d70552c0-6185-4c78-88c0-f56272678bc9")
             };
             _bikeservice.addBike(bike);
             return Ok("Bici aggiunta");
         }
         [Authorize(Roles = "Admin")]
         [HttpDelete(Name = "RemoveBike")]
-        public IActionResult DeletedBike(Guid id)
+        public IActionResult DeletedBike([FromBody] Guid id)
         {
             _bikeservice.DeleteBike(id);
             return RedirectToAction("Index");
         }
         [Authorize(Roles = "Admin")]
         [HttpPatch(Name = "LockUnlock")]
-        public IActionResult LockUnlock(Guid id)
+        public IActionResult LockUnlock([FromBody] Guid id)
         {
             _bikeservice.PatchLock(id);
             return RedirectToAction("Index");
