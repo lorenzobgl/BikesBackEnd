ed0069b237feca4ba2031396a1e5fb49d68e1bb3
riccardo <riccardo.dellavecchia@emea.nttdata.com>
2022-07-13 12:35:56 +0200

    uno-errori

---
+++ BikesBackEnd/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BikesBackEnd.Data
+{
+    public class AppDbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
